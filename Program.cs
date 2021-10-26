d6918f7fe2a962a2cfa5a1295654e60b5ae2fa53
Jonathan Nneji <jonathannn3j1@outlook.com>
2021-10-26 16:20:43 -0400

    Threading the needle

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Threading;
 
 namespace ProjectCanvas
 {
@@ -6,7 +7,7 @@ namespace ProjectCanvas
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            
         }
     }
 }
---
+++ SyncRetrival.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace ProjectCanvas
+{
+    class SyncRetrival
+    {
+
+        public void runThread()
+        {
+            Console.WriteLine("Main thread: Start a second thread.");
+            Thread t = new(new ThreadStart(ProcThread));
+
+            t.Start();
+            Thread.Sleep(100);
+
+
+            for (int i = 0; i < 4; i++)
+            {
+                Console.WriteLine("Main thread: Do some work.");
+                Thread.Sleep(100);
+            }
+
+            Console.WriteLine("Main thread: Call Join(), to wait until ThreadProc ends.");
+            t.Join();
+            Console.WriteLine("Main thread: ThreadProc.Join has returned.  Press Enter to end program.");
+            Console.ReadLine();
+        }
+
+        public static void ProcThread()
+        {
+            for(int i = 0; i < 10; i++)
+            {
+                Console.WriteLine("ThreadProc: {0}", i);
+                Thread.Sleep(100);
+            }
+        }
+
+    }
+}
